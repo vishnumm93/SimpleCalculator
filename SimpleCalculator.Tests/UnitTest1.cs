37541bf2862735f5a28c8d32987bdc72dd3a314d
vishnu <vishnumm@outlook.com>
2025-07-01 18:18:35 -0400

    Added unit tests

--- SimpleCalculator.Tests/UnitTest1.cs
+++ SimpleCalculator.Tests/UnitTest1.cs
@@ -20,5 +20,12 @@ namespace SimpleCalculator.Tests
             double result = calculator.Add(5, 3);
             Assert.AreEqual(8, result);
         }
+
+        [TestMethod]
+        public void Sub_ShouldReturnCorrectSub()
+        {
+            double result = calculator.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
     }
 }
